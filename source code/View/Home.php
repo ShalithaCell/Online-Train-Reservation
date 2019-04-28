ffa6bf1f9d2f7f9daa61a3cb0eb4be99f9691063
Shalitha Senanayaka <shalithax@gmail.com>
2019-04-28 10:28:44 +0530

    redesign home page

--- source code/View/Home.php
+++ source code/View/Home.php
@@ -46,24 +46,24 @@
             //-------------------------js methods-------------------------------------------
 
 
-            document.getElementById("btnExchange").addEventListener("click", function(event){
-                event.preventDefault()
-
-                var txtFrom = document.getElementById('txtTrainFrom').value;
-                var txtTo = document.getElementById('txtTrainTo').value;
-
-                if(txtFrom != '' && txtTo != ''){
-                    document.getElementById("txtTrainFrom").value = txtTo;
-                    document.getElementById("txtTrainTo").value = txtFrom;
-                }
-
-            });
-
-            document.getElementById("TrainSearch").addEventListener("click", function(event){
-                event.preventDefault()
-                location.href = "TicketSearch.html";
-                
-            });
+            // document.getElementById("btnExchange").addEventListener("click", function(event){
+            //     event.preventDefault()
+            //
+            //     var txtFrom = document.getElementById('txtTrainFrom').value;
+            //     var txtTo = document.getElementById('txtTrainTo').value;
+            //
+            //     if(txtFrom != '' && txtTo != ''){
+            //         document.getElementById("txtTrainFrom").value = txtTo;
+            //         document.getElementById("txtTrainTo").value = txtFrom;
+            //     }
+            //
+            // });
+            //
+            // document.getElementById("TrainSearch").addEventListener("click", function(event){
+            //     event.preventDefault()
+            //     location.href = "TicketSearch.html";
+            //
+            // });
 
 
         });
@@ -85,73 +85,138 @@
 
     <!--Page Content -->
     <div style="height:100%">
-        <div class="search-block">
-            <div>
-                <div class="row font-Sego">
-                    <div class="column-T center-block-column">
-                        <p>
-                            BOOKit Trains 24x7 Helpline <br />
-                            011-1234567
-                        </p>
-                        <div style="background-color:white">
-                            <label class="master-blue-color txt1">Your Ticket Search Ends Here</label>
-                        </div>
+
+        <div class="row child-block-style-T mt-3 search-box-margin">
+            <div class="col-4">
+                <div class="col-md-12 ">
+                <p class="half-font">
+                    BOOKit Trains 24x7 Helpline <br />
+                    011-1234567
+                </p>
+                </div>
+                <div class="col-md-12 cus-block-1">
+                <div class="mt-5">
+                    <label class="text-primary cus-font-weight">Your Ticket Search Ends Here</label>
+
+                    <div class="row">
+                    <div class="custom-control custom-radio ml-3">
+                        <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="groupOfDefaultRadios" checked>
+                        <label class="custom-control-label" for="defaultGroupExample1">Book Ticket</label>
                     </div>
-                    <div class="column-T center-blick-column-second">
-                        <p style="padding-left: 22%;">
-                            Hassle Free Train Tickets Booking <br />
-                            - Zero service charge <br />
-                            - Easy cancellation and refund <br />
-                            - No hidden charges
-                        </p>
+
+                    <!-- Group of default radios - option 2 -->
+                    <div class="custom-control custom-radio ml-3">
+                        <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="groupOfDefaultRadios" >
+                        <label class="custom-control-label" for="defaultGroupExample2">Check PNR</label>
                     </div>
-                    <div class="column-T">
-                        <img id="imgTrain" src="../resources/Train.png" style="padding-left: 22%;" />
+                    </div>
+                </div>
+                </div>
+
+            </div>
+            <div class="col-4">
+                <div>
+                    <h4>Hassle Free Train Tickets Booking</h4>
+                    <ul class="ul-cus">
+                        <li>Zero service charge</li>
+                        <li>Easy cancellation and refund</li>
+                        <li>No hidden charges</li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-4">
+                <img id="imgTrain" src="../resources/Train.png" class="img-responsive" />
+            </div>
+
+            <div class="row col-md-12 cus-block-1 ml-2">
+                <div class="row">
+                    <div class="md-form">
+                        <i class="fa fa-diamond prefix"></i>
+                        <input type="text" id="form2" class="form-control">
+                        <label for="form2" class="">From</label>
+                    </div>
+                    <div class="md-form">
+                        <i class="fa fa-diamond prefix"></i>
+                        <input type="text" id="form2" class="form-control">
+                        <label for="form2" class="">From</label>
+                    </div>
+                    <div class="md-form">
+                        <i class="fa fa-diamond prefix"></i>
+                        <input type="text" id="form2" class="form-control">
+                        <label for="form2" class="">From</label>
                     </div>
                 </div>
             </div>
-            <div>
-                <form action="">
-                    <div class="form-inline-T home-radio-block font-Sego">
-                            <label class="container-T">Book Ticket
-                                    <input type="radio" checked="checked" name="radio">
-                                    <span class="checkmark"></span>
-                            </label>
-                            &nbsp;&nbsp;&nbsp;&nbsp;
-                            <label class="container-T">Check PNR Status
-                                    <input type="radio" name="radio">
-                                    <span class="checkmark"></span>
-                            </label>
-                    </div>
-                    <div class="form-inline-T search-bar font-Sego">
-                        <!--Text box From-->
-                        <div class="search-bar-item">
-                            <label for="txtTrainFrom">From</label>
-                            <input type="text" id="txtTrainFrom" name="txtbFrom">
-                        </div>
-                        <div>
-                                <button id="btnExchange" ><i class="fas fa-exchange-alt"></i></button>
-                        </div>
-                        <!--Text box To-->
-                        <div class="search-bar-item" style="margin-left: 2%;">
-                                <label for="txtTrainTo">To</label>
-                                <input type="text" id="txtTrainTo" name="txtbTo">
-                            </div>
-                            <div class="search-bar-item calender" style="margin-left: 2%;">
-                                    <label for="dPicker">Date</label>
-                                    <input type="date" name="Date" id="dPicker">
-                            </div>
-                            <button class="button-T" id="TrainSearch" style="margin-left: 2%;">Search</button>
-                    </div>
-                    </div>
-                </form>
+        </div>
+
+<!--        <div class="search-block">-->
+<!--            <div>-->
+<!--                <div class="row font-Sego">-->
+<!--                    <div class="column-T center-block-column">-->
+<!--                        <p>-->
+<!--                            BOOKit Trains 24x7 Helpline <br />-->
+<!--                            011-1234567-->
+<!--                        </p>-->
+<!--                        <div style="background-color:white">-->
+<!--                            <label class="master-blue-color txt1">Your Ticket Search Ends Here</label>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="column-T center-blick-column-second">-->
+<!--                        <p style="padding-left: 22%;">-->
+<!--                            Hassle Free Train Tickets Booking <br />-->
+<!--                            - Zero service charge <br />-->
+<!--                            - Easy cancellation and refund <br />-->
+<!--                            - No hidden charges-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                    <div class="column-T">-->
+<!--                        <img id="imgTrain" src="../resources/Train.png" style="padding-left: 22%;" />-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div>-->
+<!--                <form action="">-->
+<!--                    <div class="form-inline-T home-radio-block font-Sego">-->
+<!--                            <label class="container-T">Book Ticket-->
+<!--                                    <input type="radio" checked="checked" name="radio">-->
+<!--                                    <span class="checkmark"></span>-->
+<!--                            </label>-->
+<!--                            &nbsp;&nbsp;&nbsp;&nbsp;-->
+<!--                            <label class="container-T">Check PNR Status-->
+<!--                                    <input type="radio" name="radio">-->
+<!--                                    <span class="checkmark"></span>-->
+<!--                            </label>-->
+<!--                    </div>-->
+<!--                    <div class="form-inline-T search-bar font-Sego">-->
+<!--                        Text box From-->
+<!--                        <div class="search-bar-item">-->
+<!--                            <label for="txtTrainFrom">From</label>-->
+<!--                            <input type="text" id="txtTrainFrom" name="txtbFrom">-->
+<!--                        </div>-->
+<!--                        <div>-->
+<!--                                <button id="btnExchange" ><i class="fas fa-exchange-alt"></i></button>-->
+<!--                        </div>-->
+<!--                        Text box To-->
+<!--                        <div class="search-bar-item" style="margin-left: 2%;">-->
+<!--                                <label for="txtTrainTo">To</label>-->
+<!--                                <input type="text" id="txtTrainTo" name="txtbTo">-->
+<!--                            </div>-->
+<!--                            <div class="search-bar-item calender" style="margin-left: 2%;">-->
+<!--                                    <label for="dPicker">Date</label>-->
+<!--                                    <input type="date" name="Date" id="dPicker">-->
+<!--                            </div>-->
+<!--                            <button class="button-T" id="TrainSearch" style="margin-left: 2%;">Search</button>-->
+<!--                    </div>-->
+<!--                    </div>-->
+<!--                </form>-->
+<!--            </div>-->
+
+            <div class="container img-responsive">
+                <img src="../resources/2019-03-10 13_56_41-Book Train, Bus & Flights_ Confirmtkt.com.png" class="img-responsive" style="width: 100% !important;">
             </div>
 
-            <div>
-                <img src="../resources/2019-03-10 13_56_41-Book Train, Bus & Flights_ Confirmtkt.com.png" class="responsive-custom image-suport">
-            </div>
-
-            <div>
+            <div class="container">
                 <!-- Flickity HTML init -->
                 <div class="carousel js-flickity">
                     <div class="carousel-cell">
@@ -168,7 +233,7 @@
             </div>
             
         </div>
-    </div>
+
 
     <!--Footer -->
     <footer id="footerID"></footer>
@@ -176,8 +241,10 @@
     <!--cannot read property 'addeventlistener' of null mdb
 - This is probably because the script is executed before the page loads. By placing the script at the bottom of the page, I circumvented the problem.
 -->
-    <script src="../ExternalResources/MDB/js/mdb.min.js"></script>
+
 
 </body>
+
+<script src="../ExternalResources/MDB/js/mdb.min.js"></script>
 
 </html>
