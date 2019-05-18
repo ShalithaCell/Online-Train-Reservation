fd3ffde0d5803eb4ac80e0891d77c5aedc243f33
user1 <user1@localhost.com>
2019-05-18 17:12:42 +0530

    Station page by gayan

---
+++ source code/Script/faq.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
